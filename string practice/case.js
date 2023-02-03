df2e59839268efb57a351c013f80384f8a6e25a5
rudradcruze <francisrudra@gmail.com>
2023-02-03 09:46:32 +0600

    string search

---
+++ string practice/case.js
@@ -0,0 +1,15 @@
+const userName1 = "HelloHiiI";
+const userName2 = "HelloHiii";
+
+if (userName1.toLowerCase() === userName2.toLowerCase())
+    console.log("Valid User");
+else
+    console.log("Invalid User");
+
+if (userName1 === userName2)
+    console.log("Valid User");
+else
+    console.log("Invalid User");
+
+console.log(userName1.toLowerCase());
+console.log(userName2.toLowerCase());
